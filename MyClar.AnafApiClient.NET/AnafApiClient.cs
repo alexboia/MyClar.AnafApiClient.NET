70b29ee6a5396b35b193408b434ce42e89913768
Alexandru Boia <alexandru.boia@outlook.com>
2023-09-15 14:44:48 +0300

    Added a class to server as assembly marker.

---
+++ MyClar.AnafApiClient.NET/AnafApiClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClar.AnafApiClient.NET
+{
+	//Marker class for this library
+	public class AnafApiClient
+	{
+		public static Assembly Assembly => typeof( AnafApiClient ).Assembly;
+	}
+}
